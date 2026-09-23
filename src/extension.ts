f20f011f1d033350cfc982bebeba15e4bd6ab7d4
Dimitri Kanjuka <d.kanjuka@seznam.cz>
2026-09-23 21:06:02 +0200

    Add hello command and F5 debug configuration

--- src/extension.ts
+++ src/extension.ts
@@ -4,6 +4,13 @@ export function activate(context: vscode.ExtensionContext): void {
   const output = vscode.window.createOutputChannel('Dimicek', { log: true });
   context.subscriptions.push(output);
   output.info('Dimicek activated');
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand('dimicek.hello', () => {
+      output.info('Hello command executed');
+      void vscode.window.showInformationMessage('Hello from Dimicek!');
+    }),
+  );
 }
 
 export function deactivate(): void {}
